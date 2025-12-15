34a22ce98901be1631b45c8e2b6413ee6bbac093
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-15 08:01:11 +0530

    adding files on 15 dec

---
+++ src/ex_02_Java_Basics/$.java
@@ -0,0 +1,7 @@
+package ex_02_Java_Basics;
+
+public class $ {
+    public static void main(String[] args) {
+        System.out.println("new name of class ");
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab_Math2.java
@@ -0,0 +1,7 @@
+package ex_02_Java_Basics;
+
+public class Lab_Math2 {
+    public static void main(String[] args) {
+        System.out.println("2+2");
+    }
+}
