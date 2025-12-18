8f4628b1ca6363597b8b1eee16c35ee4feb25548
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-18 14:51:54 +0530

    adding new Java file for learning math operation

---
+++ src/ex_03_Java_Basics/MathJava.java
@@ -0,0 +1,16 @@
+package ex_03_Java_Basics;
+//this is for explaining math concepts
+
+import java.sql.SQLOutput;
+
+/**
+ * This is multiline comment
+ **/
+public class MathJava {
+    public static void main(String[] args) {
+        System.out.println("single line comment and mutliline comment ");
+        System.out.println(2+2);
+        System.out.println("9+4");
+    }
+
+}
