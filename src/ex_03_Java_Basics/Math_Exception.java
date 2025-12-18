7794c2efc8cefaedccb4b4152f43cce8bcf1a402
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-18 15:27:35 +0530

    adding new Java file for math exceptions

---
+++ src/ex_03_Java_Basics/Math_Exception.java
@@ -0,0 +1,11 @@
+package ex_03_Java_Basics;
+
+public class Math_Exception {
+    public static void main(String[] args) {
+        //System.out.println(10/0); not supported
+        System.out.println(0/100);
+        System.out.println(10/10);
+        System.out.println(0/20);
+
+    }
+}
