add4805a264aefa379908e6697f46640669ad343
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-18 15:17:08 +0530

    adding new Java file for learning keywords

---
+++ src/ex_03_Java_Basics/Keyword.java
@@ -0,0 +1,8 @@
+package ex_03_Java_Basics;
+
+public class Keyword {
+    public static void main(String[] args) {
+        System.out.println("keywords");
+        //public,package,class, static,void -all are keywords (lower case)
+    }
+}
