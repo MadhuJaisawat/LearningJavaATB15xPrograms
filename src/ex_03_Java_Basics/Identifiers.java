a4044dafdf63b7e65ed66c0963b603d4c86fd66a
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-18 15:21:00 +0530

    adding new Java file for learning identifiers

---
+++ src/ex_03_Java_Basics/Identifiers.java
@@ -0,0 +1,7 @@
+package ex_03_Java_Basics;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Identifiers- name of elements of programming language eg. class,method ,variable name");
+    }
+}
