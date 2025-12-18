fdac611290dc058742ab53390f13475d8063e3de
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-18 15:32:34 +0530

    adding fill for understanding diff between Println and print

---
+++ src/ex_03_Java_Basics/Diff_Println_VS_Print.java
@@ -0,0 +1,12 @@
+package ex_03_Java_Basics;
+
+public class Diff_Println_VS_Print {
+    public static void main(String[] args) {
+        System.out.println("My name is");
+        System.out.println("Madhu");
+        //Printlm will add a new line
+        System.out.print("My name is");
+        System.out.print("Madhu");
+
+    }
+}
