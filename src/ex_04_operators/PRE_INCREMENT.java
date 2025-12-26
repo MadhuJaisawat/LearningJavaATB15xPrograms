32843e0a68155f225e11c4bb891dd53b72655a28
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2025-12-26 12:39:21 +0530

    Adding Files on 25 Dec

---
+++ src/ex_04_operators/PRE_INCREMENT.java
@@ -0,0 +1,10 @@
+package ex_04_operators;
+
+public class PRE_INCREMENT {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = ++a;
+        //increase value by 1 means a will be 11
+        System.out.println();
+    }
+}
