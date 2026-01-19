63c656b80efe2b786fe6f55ce13f44e93943e9d8
Madhu Jaisawat <madhu.bhadauriaomatic@gmail.com>
2026-01-19 13:01:48 +0530

    Reverse a String program

--- src/ex_17_Strings/reverseAStringProgm.java
+++ src/ex_17_Strings/reverseAStringProgm.java
@@ -6,7 +6,7 @@ public class reverseAStringProgm {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the String");
-        String input =sc.next();
+        String input =sc.nextLine();
         String reverse = "";
 
         for (int i=input.length()-1;i>=0;i--) {
